285b0eef1e75f44562bc9842bcb072847611082f
karani-ken <academice79@gmail.com>
2023-08-21 21:59:43 +0300

    Add project files.

---
+++ Tests/HelloTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Testing.Tests
+{
+    public class HelloTest
+    {
+        public string Greet()
+        {
+            return "Hello Test"; 
+        }
+    }
+}
---
+++ Tests/Trainees.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace Unit_Testing.Tests
+{
+    public class Trainees
+    {
+        private  List<string> _studentList = new List<string>();
+
+         public int student_count => _studentList.Count;
+
+        public void addStudent(string studentName)
+        {
+            //nullChecking
+            if (string.IsNullOrEmpty(studentName))
+                throw new ArgumentNullException("name is required");
+            _studentList.Add(studentName);
+        }
+        public void removeStudent(string studentName)
+        {
+            //nullChecking
+            if (string.IsNullOrEmpty(studentName))
+                throw new ArgumentNullException("name is required");
+            _studentList.Remove(studentName);
+        }
+        public string searchStudent(string studentName)
+        {
+            //nullChecking
+            if (string.IsNullOrEmpty(studentName))
+                throw new ArgumentNullException("name is required");
+            var name = _studentList.Find(x => x == studentName);
+            if(name != null)
+            {
+                return name;
+            }
+            return string.Empty;
+        }
+    }
+}
