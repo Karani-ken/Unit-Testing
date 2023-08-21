c2e66a3e83bfe442db07732f915db1c67628900e
karani-ken <academice79@gmail.com>
2023-08-21 22:35:11 +0300

    to be continued

--- Tests/Trainees.cs
+++ Tests/Trainees.cs
@@ -7,12 +7,22 @@ namespace Unit_Testing.Tests
         private  List<string> _studentList = new List<string>();
 
          public int student_count => _studentList.Count;
+        //if input is string - add student
+        //if name exists -throw an error
 
+        //if input is ""
+        //if name is null -- throw an error
+        //if input is "   " whitespaces -- throw an error
         public void addStudent(string studentName)
         {
             //nullChecking
-            if (string.IsNullOrEmpty(studentName))
+            if (string.IsNullOrWhiteSpace(studentName))
                 throw new ArgumentNullException("name is required");
+            var student = _studentList.Find(x => x == studentName);
+            if (student != null)
+            {
+                throw new InvalidOperationException("You can't add the name twice");
+            }
             _studentList.Add(studentName);
         }
         public void removeStudent(string studentName)
